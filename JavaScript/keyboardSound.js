e0f9addcf9e35990cd87fd294a113c3c516773d1
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-10-29 13:52:00 -0300

    Delete JavaScript directory

--- JavaScript/keyboardSound.js
+++
@@ -1,7 +0,0 @@
-function playKeyboardSound()
-{
-    var audio = new Audio("./JavaScript/Audio/MarcosVinicius.mp3");
-    audio.play();
-}
-
-document.addEventListener("keydown", playKeyboardSound);
