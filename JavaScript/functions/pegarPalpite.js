c5e39f2b585ef79ef3c52ca1ee6889c7b62a1c2c
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-11-15 19:19:10 -0300

    Adicionar JSDoc para a função pegarPalpite

--- JavaScript/functions/pegarPalpite.js
+++ JavaScript/functions/pegarPalpite.js
@@ -1,5 +1,9 @@
 export function pegarPalpite()
 {
+    /**
+     * Essa função pega os valores exibidos na tela, dentro dos 5 grids, forma uma string e a retorna como uma palavra.
+    */
+
     var palpite = "";  // Palavra digitada pelo jogador
     var grids = document.querySelectorAll(".grid-item.enabled");
 
