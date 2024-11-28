cb2a6599a656c1458695d5aefe5db2ddf144852e
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-11-28 16:38:36 -0300

    Impedir de digitar palavras menores que 5 letras

--- JavaScript/functions/inputTecladoFisico.js
+++ JavaScript/functions/inputTecladoFisico.js
@@ -2,29 +2,35 @@ import { somTecla } from "./somTecla.js";
 import { mostrarNaTela } from "./mostrarNaTela.js";
 import { apagarDaTela } from "./apagarDaTela.js";
 import { logicaDoJogo } from "./logicaDoJogo.js";
+import { verificarTamanhoDaPalavra } from "./verificarTamanhoDaPalavra.js";
 
-const regex = /^[a-zA-Z]$/; // Vai funcinar como nosso filtro de letras
-const teclasPermitidas = ['enter', 'backspace']; // Teclas especiais permitidas
+const regex = /^[a-zA-Z]$/;
+const teclasPermitidas = ['enter', 'backspace'];
 
-// As mudanças aqui foram grandes ;)
-document.addEventListener('keydown', (tecla) => { // Preferi fazer uma Arrow Function porque quero adicionar o Input com Ctrl+V dps...
+document.addEventListener('keydown', (tecla) => { 
 
     let input = tecla.key.toLowerCase();
 
-    if (!regex.test(input) && !teclasPermitidas.includes(input)) tecla.preventDefault(); // Aqui é onde ocorre o teste do filtro regex, caso o ususario digite algo fora dele ele bloqueia a entrada
-    else{ // Aqui continua bem semelhante ao seu código
+    if (!regex.test(input) && !teclasPermitidas.includes(input)) tecla.preventDefault();
+    else{
 
         somTecla();
 
-        if (input == "backspace") {
+        if (input == "backspace")
+        {
             apagarDaTela();
 
-        } else if (input == "enter") {
-            logicaDoJogo();
-
-        } else {
+        }
+        else if (input == "enter")
+        {
+            if (verificarTamanhoDaPalavra())
+            {
+                logicaDoJogo();
+            }
+        }
+        else
+        {
             mostrarNaTela(input);
-
         }
         
     }
