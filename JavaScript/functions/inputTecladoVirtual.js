de9091f749dd12c3e80b98eea119a4a367e4761a
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-11-28 16:39:07 -0300

    Impedir de digitar palavras menores que 5 letras

--- JavaScript/functions/inputTecladoVirtual.js
+++ JavaScript/functions/inputTecladoVirtual.js
@@ -2,6 +2,7 @@ import { somTecla } from "./somTecla.js";
 import { mostrarNaTela } from "./mostrarNaTela.js";
 import { apagarDaTela } from "./apagarDaTela.js";
 import { logicaDoJogo } from "./logicaDoJogo.js";
+import { verificarTamanhoDaPalavra } from "./verificarTamanhoDaPalavra.js";
 
 
 function inputTecladoVirtual(tecla)
@@ -14,7 +15,11 @@ function inputTecladoVirtual(tecla)
     }
     else if (tecla == "Enter")
     {
-        logicaDoJogo();
+        if (verificarTamanhoDaPalavra())
+        {
+            logicaDoJogo();
+        }
+
     }
     else
     {
