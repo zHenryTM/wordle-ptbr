554315f5ce8b45a3dfed0ff688b729fccd7ad74f
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-12-05 21:05:47 -0300

    Método para remover acentos adicionado
    A lógica do jogo não consegue validar letras que possuam acentos gráficos, portanto, é necessário remover esses acentos para validar as palavras e, por esse motivo, foi-se criado o método removerAcentosGraficos(palavra).

--- JavaScript/functions/obterPalavraAleatoria.js
+++ JavaScript/functions/obterPalavraAleatoria.js
@@ -1,5 +1,6 @@
 var listaDePalavras;
 var palavraAleatoria;
+var palavraAleatoriaSemAcento;
 var gist_url = "https://gist.githubusercontent.com/un-versed/6373912fbf4649704b6823ea696cfcb1/raw/629137a0d0c7160b94c35013df8d570b31100174/termooo-wordsv2.json";
 
 
@@ -12,9 +13,10 @@ window.onload = async function()
         .then(listaDePalavras => listaDePalavras.split(","));
 
     palavraAleatoria = tratarPalavraAleatoria(listaDePalavras[Math.floor(Math.random() * listaDePalavras.length)]);
+    palavraAleatoriaSemAcento = removerAcentosGraficos(palavraAleatoria);
 
     console.log(palavraAleatoria);
-    console.log(palavraAleatoria.length);
+    console.log(palavraAleatoriaSemAcento);
 }
 
 
@@ -33,9 +35,45 @@ function tratarPalavraAleatoria(palavraAleatoria)
 }
 
 
+function removerAcentosGraficos(palavra)
+{
+    var palavraSemAcento = "";
+    var acentosGraficos =
+    {
+        "Á": "A",
+        "Â": "A",
+        "Ã": "A",
+        "É": "E",
+        "Ê": "E",
+        "Í": "I",
+        "Ó": "O",
+        "Ô": "O",
+        "Õ": "O",
+        "Ú": "U",
+        "Ç": "C"
+    };
+
+    for (var i = 0; i < palavra.length; i++)
+    {
+        var letra = palavra[i];
+
+        if (Object.keys(acentosGraficos).includes(letra))
+        {
+            palavraSemAcento += acentosGraficos[letra];
+        }
+        else
+        {
+            palavraSemAcento += letra;
+        }
+    }
+
+    return palavraSemAcento;
+}
+
+
 export function obterPalavraAleatoria()
 {
     // Retorna uma palavra de 5 caracteres
 
-    return palavraAleatoria;
+    return palavraAleatoriaSemAcento;
 }
