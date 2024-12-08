fb0495c7be0a327e82fc0b091be1859c23ea0b40
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-12-08 20:37:40 -0300

    Trocar cor do teclado virutal conforme a cor dos grids

---
+++ JavaScript/functions/trocarCorDaTeclaVirtual.js
@@ -0,0 +1,25 @@
+export function trocarCorDaTeclaVirtual(letra, cor)
+{
+    /**
+     * Ele troca a cor da tecla do teclado virtual conforme a cor do grid em que essa letra está presente
+    */
+
+    var teclasObject = document.querySelectorAll(".key");
+    var teclasArray = Object.keys(teclasObject).map(tecla => teclasObject[tecla]);
+    var tecla = teclasArray.filter(tecla => tecla.innerHTML == letra)[0];
+    var cor = (cor == "verde") ? "right-position" : (cor == "amarelo") ? "wrong-position" : "opacity50";
+
+    if (tecla.classList.value == "key")
+    {
+        // Não há nenhuma cor na tecla virtual
+
+        tecla.classList.add(cor);
+    }
+    else if (tecla.classList.value == "key wrong-position" && cor == "right-position")
+    {
+        // Se entrar aqui, significa que a tecla virtual está amarela e será trocada para verde
+
+        tecla.classList.remove("wrong-position");
+        tecla.classList.add(cor);
+    }
+}
