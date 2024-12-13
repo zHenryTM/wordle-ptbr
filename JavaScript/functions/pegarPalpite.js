b7d19047e205ebc5f93350c3faec6aaae400c4f6
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-12-12 21:02:16 -0300

    Verificar se a lista de palavras contém o palpite do jogador

--- JavaScript/functions/pegarPalpite.js
+++ JavaScript/functions/pegarPalpite.js
@@ -1,16 +1,30 @@
+import { obterListaDePalavras } from "./obterPalavraAleatoria.js";
+import { tratarPalavraAleatoria } from "./tratarPalavraAleatoria.js";
+import { removerAcentosGraficos } from "./removerAcentosGraficos.js";
+
+
 export function pegarPalpite()
 {
     /**
-     * Essa função pega os valores exibidos na tela, dentro dos 5 grids, forma uma string e a retorna como uma palavra.
+     * Essa função pega os valores exibidos nos grids, forma uma string, verifica se a lista de palavra contém essa string e a retorna se tiver.
     */
 
     var palpite = "";  // Palavra digitada pelo jogador
     var grids = document.querySelectorAll(".grid-item.enabled");
+    var listaDePalavras = obterListaDePalavras();
+    var palpiteEmListaDePalavras = false;  // Verifica se o palpite do jogador é uma das palavras contidas na lista de palavras
 
     grids.forEach(function (grid)
     {
         palpite += grid.innerHTML;
     });
 
-    return palpite.toUpperCase();
+    listaDePalavras.forEach(palavra =>
+    {
+        palavra = tratarPalavraAleatoria(palavra);
+        palavra = removerAcentosGraficos(palavra);
+        if (palpite == palavra) palpiteEmListaDePalavras = true;
+    });
+
+    if (palpiteEmListaDePalavras) return palpite;
 }
