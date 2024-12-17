781e0b9712bf4ef25a91dc279dc105e7e6d9a107
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-12-16 22:06:05 -0300

    Update logicaDoJogo.js

--- JavaScript/functions/logicaDoJogo.js
+++ JavaScript/functions/logicaDoJogo.js
@@ -4,7 +4,6 @@ import { trocarCorDoGrid } from "./trocarCorDoGrid.js";
 import { trocarGrids } from "./trocarGrids.js";
 import { trocarCorDaTeclaVirtual } from "./trocarCorDaTeclaVirtual.js";
 import { desabilitarGridsHabilitados } from "./desabilitarGridsHabilitados.js";
-import { flipAnimation } from "./flipAnimation.js";
  
 
 function indexOfLetter(palavra, letra)
@@ -78,13 +77,11 @@ function compararLetras(objeto_palavra, objeto_palpite, chavesIguais)
             {
                 if (letraPalavra.includes(letra))
                 {
-                    flipAnimation();
                     trocarCorDoGrid(letra, "verde");
                     trocarCorDaTeclaVirtual(chave, "verde")
                 }
                 else
                 {
-                    flipAnimation();
                     trocarCorDoGrid(letra, "amarelo");
                     trocarCorDaTeclaVirtual(chave, "amarelo")
                 }
