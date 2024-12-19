ce6a93732bebe8c03e9650e7e89fd3a168b4caee
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-12-19 13:17:37 -0300

    Update somTecla.js

--- JavaScript/functions/somTecla.js
+++ JavaScript/functions/somTecla.js
@@ -1,6 +1,6 @@
 export function somTecla()
 {
-    let audio = new Audio("./JavaScript/Audio/MarcosVinicius.mp3");
+    let audio = new Audio("./../../Assets/Audio/MarcosVinicius.mp3");
     audio.volume = 0.3;
     audio.play();
 }
