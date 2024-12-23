320202a28e42a001e6455a63c5d20486a9036f55
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-12-23 17:49:10 -0300

    Update inputTecladoFisico.js

--- JavaScript/functions/inputTecladoFisico.js
+++ JavaScript/functions/inputTecladoFisico.js
@@ -3,36 +3,41 @@ import { mostrarNaTela } from "./mostrarNaTela.js";
 import { apagarDaTela } from "./apagarDaTela.js";
 import { logicaDoJogo } from "./logicaDoJogo.js";
 import { verificarTamanhoDaPalavra } from "./verificarTamanhoDaPalavra.js";
+import { Menu } from "./../menu.js";
+
 
 const regex = /^[a-zA-Z]$/;
 const teclasPermitidas = ['enter', 'backspace'];
 
 document.addEventListener('keydown', (tecla) => { 
 
-    let input = tecla.key.toLowerCase();
+    if (!Menu.instrucoesEstaVisivel && !Menu.configuracoesEstaVisivel)
+    {
+        let input = tecla.key.toLowerCase();
 
-    if (!regex.test(input) && !teclasPermitidas.includes(input)) tecla.preventDefault();
-    else{
+        if (!regex.test(input) && !teclasPermitidas.includes(input)) tecla.preventDefault();
+        else{
 
-        somTecla();
+            somTecla();
 
-        if (input == "backspace")
-        {
-            apagarDaTela();
-
-        }
-        else if (input == "enter")
-        {
-            if (verificarTamanhoDaPalavra())
+            if (input == "backspace")
             {
-                logicaDoJogo();
+                apagarDaTela();
+
             }
+            else if (input == "enter")
+            {
+                if (verificarTamanhoDaPalavra())
+                {
+                    logicaDoJogo();
+                }
+            }
+            else
+            {
+                mostrarNaTela(input);
+            }
+            
         }
-        else
-        {
-            mostrarNaTela(input);
-        }
-        
     }
 
-})
+});
