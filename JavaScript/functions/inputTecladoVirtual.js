abc9d571a4f9c9eef049a86091e71962128d15d0
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-12-23 17:49:36 -0300

    Update inputTecladoVirtual.js

--- JavaScript/functions/inputTecladoVirtual.js
+++ JavaScript/functions/inputTecladoVirtual.js
@@ -3,27 +3,31 @@ import { mostrarNaTela } from "./mostrarNaTela.js";
 import { apagarDaTela } from "./apagarDaTela.js";
 import { logicaDoJogo } from "./logicaDoJogo.js";
 import { verificarTamanhoDaPalavra } from "./verificarTamanhoDaPalavra.js";
+import { Menu } from "./../menu.js";
 
 
 function inputTecladoVirtual(tecla)
 {
-    somTecla();
+    if (!Menu.instrucoesEstaVisivel && !Menu.configuracoesEstaVisivel)
+    {
+        somTecla();
 
-    if (tecla == "Backspace")
-    {
-        apagarDaTela();
-    }
-    else if (tecla == "Enter")
-    {
-        if (verificarTamanhoDaPalavra())
+        if (tecla == "Backspace")
         {
-            logicaDoJogo();
+            apagarDaTela();
         }
+        else if (tecla == "Enter")
+        {
+            if (verificarTamanhoDaPalavra())
+            {
+                logicaDoJogo();
+            }
 
-    }
-    else
-    {
-        mostrarNaTela(tecla);
+        }
+        else
+        {
+            mostrarNaTela(tecla);
+        }
     }
 }
 
