b24d23d666b4cef2c7bb4509bf69abad0fbf683c
Henrique Angelin <81445180+zHenryTM@users.noreply.github.com>
2024-12-26 22:02:05 -0300

    Create copiarLinkDoSite.js

---
+++ JavaScript/functions/copiarLinkDoSite.js
@@ -0,0 +1,9 @@
+export function copiarLinkDoSite()
+{
+    var link = "https://zhenrytm.github.io/wordle-ptbr/";
+
+    navigator.clipboard.writeText(link);
+}
+
+
+window.copiarLinkDoSite = copiarLinkDoSite;
